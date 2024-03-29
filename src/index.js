4ffece96e4b6df143bf63c978421bbc359bace00
Camille L <camille0756@gmail.com>
2024-03-29 16:58:51 +0100

    Pagelayout and error page removal after deploying single page on github page

--- src/index.js
+++ src/index.js
@@ -5,20 +5,18 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home/Home';
-import NotFound from './pages/NotFound/NotFound';
-import PageLayout from './components/PageLayout/PageLayout';
-
+import Header from '../src/components/Header/Header';
+import Footer from '../src/components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
+      <Header />
         <Routes>
-        <Route element={<PageLayout />}>
-        <Route path="/" element={<Home/>} />
-        </Route>
-        <Route path="*" element={<NotFound/>} />
+          <Route path="/" element={<Home/>} />
         </Routes>
+      <Footer />
     </Router>
   </React.StrictMode>
 );
